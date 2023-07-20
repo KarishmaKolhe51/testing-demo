1daaf8634d24c6bd5e739bf7694c8930c5eec9d2
Karishma Kolhe <karishmakolhe49@gmail.com>
2023-07-21 01:01:20 +0530

    first commit

---
+++ src/main/java/com/cdac/Calculator.java
@@ -0,0 +1,16 @@
+package com.cdac;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator 
+{
+    public int add(int x, int y) {
+            return x + y;
+        }
+    
+        public int sub(int x, int y) {
+            return x - y;
+        }
+}
---
+++ src/test/java/com/cdac/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.cdac;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
